7e41ab382546db32a6bb2a18b6a7e0572107cb5e
Priyanka Suri <priyankasuri1504@gmail.com>
2025-04-03 14:49:04 +0530

    Added external CSS for styling

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,16 @@
 import streamlit as st
 
+
+# Loading external CSS
+def load_css(file_name):
+    with open(file_name, "r") as f:
+        css = f.read()
+    return f"<style>{css}</style>"
+
+
+# Applying CSS styles
+st.markdown(load_css("styles.css"), unsafe_allow_html=True)
+
+
 st.title("AuraLens")
 st.write("This app analyzes emotions in tweets!")

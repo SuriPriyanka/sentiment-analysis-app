15f18471bf76cdca5181ab305351e21f4a79293d
Priyanka Suri <priyankasuri1504@gmail.com>
2025-04-03 19:07:20 +0530

    Added sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 
 # Defining inline CSS
 inline_css = """
@@ -31,3 +32,16 @@ st.markdown(inline_css, unsafe_allow_html=True)
 #Frontend View
 st.title("AuraLens")
 st.write("This app analyzes emotions in tweets!")
+
+
+# Sidebar styling
+st.set_page_config(page_title="Dashboard", page_icon="🏠", layout="wide")
+
+with st.sidebar:
+    selected = option_menu(
+        menu_title=None,
+        options=["Home", "Profile", "Notifications", "Settings", "Add another account", "Sign out", "Plans and Pricing", "Report Content", "Privacy Policy", "Suggest Improvement"],
+        icons=["house", "person", "bell", "gear", "plus-circle", "box-arrow-right", "currency-dollar", "flag", "shield-lock", "lightbulb"],
+        menu_icon="list",  # Icon for the menu
+        default_index=0
+    )

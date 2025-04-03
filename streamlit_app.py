f98204870984b48c8d8d5c8266dc1ee5b1f01395
Priyanka Suri <priyankasuri1504@gmail.com>
2025-04-03 15:37:17 +0530

    Fixed styles.css issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,14 @@
 import streamlit as st
+import os
 
 
 # Loading external CSS
 def load_css(file_name):
+    css_path = os.path.join(os.path.dirname(__file__), "styles.css")
+    if os.path.exists(css_path):
+      st.write("styles.css found!")
+    else:
+      st.write("styles.css NOT found!")
     with open(file_name, "r") as f:
         css = f.read()
     return f"<style>{css}</style>"
